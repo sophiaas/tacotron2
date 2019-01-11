d771ece8336e29448e58de45d8352b6b8749f947
sophia <sophia.sanborn@gmail.com>
2019-01-11 10:09:30 -0800

    param changes for new dataset

--- hparams.py
+++ hparams.py
@@ -24,15 +24,16 @@ def create_hparams(hparams_string=None, verbose=False):
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='filelists/ljs_audio_text_train_filelist.txt',
-        validation_files='filelists/ljs_audio_text_val_filelist.txt',
+        dataset_name='m_ailabs',
+        training_files='filelists/m_ailabs/mary_ann/ma_audio_text_train_filelist.csv',
+        validation_files='filelists/m_ailabs/mary_ann/ma_audio_text_val_filelist.csv',
         text_cleaners=['english_cleaners'],
 
         ################################
         # Audio Parameters             #
         ################################
         max_wav_value=32768.0,
-        sampling_rate=22050,
+        sampling_rate=16000,
         filter_length=1024,
         hop_length=256,
         win_length=1024,
@@ -80,7 +81,7 @@ def create_hparams(hparams_string=None, verbose=False):
         learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
-        batch_size=64,
+        batch_size=16,
         mask_padding=True  # set model's padded outputs to padded values
     )
 
